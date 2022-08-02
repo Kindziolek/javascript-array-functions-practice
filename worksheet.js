2cffcfaf2aef8087a9f77948eba3e855d5dcf8d9
Kindziolek <kingapietron@gmail.com>
2022-08-03 00:11:55 +0200

    task

--- worksheet.js
+++ worksheet.js
@@ -154,7 +154,14 @@ const allMassMore40 = characters.every((character) => character.mass > 40);
 console.log(allMassMore40);
 
 //3. Is every character shorter than 200?
+
+const allShorterThan200 = characters.every((character) => character.height > 200);
+console.log(allShorterThan200);
+
 //4. Is every character male?
+
+const allMale = characters.every((character) => character.gender === 'male');
+console.log(allMale);
 
 //***SOME***
 //1. Is there at least one male character?
